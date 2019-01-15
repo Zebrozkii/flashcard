5447d0bb6907fe6495960cc592eae3f45c5c7168
Jimmy Zebroski and Kenny Halim <kenny@>
2019-01-15 12:02:18 -0800

    add panels and scripts

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $(".js-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".oper-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".var-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $(".func-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable4").click(function() {
+    $(".meth-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable5").click(function() {
+    $(".arg-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable6").click(function() {
+    $(".para-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable7").click(function() {
+    $(".strings-hidden").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable8").click(function() {
+    $(".bool-hidden").slideToggle();
+  });
+});
